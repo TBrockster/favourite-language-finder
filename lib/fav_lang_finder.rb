a0a916fb0da1a61ea86816e1bb50481c94c21c7f
TBrockster <tobybrock93@gmail.com>
2019-09-26 12:08:05 +0100

    refactor of previous commit

--- lib/fav_lang_finder.rb
+++ lib/fav_lang_finder.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
+# this class will use GitHub's api to find a user's most-used language
 class FavLangFinder
-  def find_fav_lang(username)
-  end
+  def find_fav_lang(username); end
 end
