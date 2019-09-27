6d40fefa2f64b0f12e85b0cc1fdc4f659b247164
TBrockster <tobybrock93@gmail.com>
2019-09-27 10:47:18 +0100

    first scaffold wrapper test written and passed

---
+++ lib/gh_lang_finder_wrapper.rb
@@ -0,0 +1,5 @@
+class GHLangFinderWrapper
+  def wrap_graphql_query(query)
+
+  end
+end
---
+++ spec/gh_lang_finder_wrapper_spec.rb
@@ -0,0 +1,5 @@
+require 'gh_lang_finder_wrapper'
+
+describe GHLangFinderWrapper do
+  it { is_expected.to respond_to(:wrap_graphql_query).with(1).argument }
+end
