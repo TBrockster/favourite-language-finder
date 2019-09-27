f8f2bfaa644094183ba8a5b9b8274daf0c85c437
TBrockster <tobybrock93@gmail.com>
2019-09-27 10:40:47 +0100

    renamed classes and files

--- lib/graphql_querier.rb
+++ lib/graphql_querier.rb
@@ -3,7 +3,7 @@
 require 'gh_lang_finder_module.rb'
 
 # this class uses a graphql library to send a query to github.
-class FavLangFinder
+class GraphQlQuerier
   include GHLangFinder
   def send_graphql_query(username, graphql_library: GHLangFinder::Client)
     graphql_library.query(GHLangBreakdown, variables: { login: username })
--- spec/graphql_querier_spec.rb
+++ spec/graphql_querier_spec.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-require 'fav_lang_finder'
+require 'graphql_querier'
 
-describe FavLangFinder do
-  subject(:fav_lang_finder) { described_class.new }
+describe GraphQlQuerier do
+  subject(:graphql_querier) { described_class.new }
   let(:fake_graphql_library) { double :fake_graphql_library }
 
   describe '#send_graphql_query' do
     it { is_expected.to respond_to(:send_graphql_query).with(1).argument }
     it 'input double receives the .query method' do
       expect(fake_graphql_library).to receive(:query)
-      fav_lang_finder.send_graphql_query('TBrockster', graphql_library: fake_graphql_library)
+      graphql_querier.send_graphql_query('TBrockster', graphql_library: fake_graphql_library)
     end
   end
 end
