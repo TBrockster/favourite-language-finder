f4ccfc59d8eb7d7120e9e1266fb2c59a9b3dedfe
TBrockster <tobybrock93@gmail.com>
2019-09-27 23:32:10 +0100

    results view now shows language, and is tabulated.

--- spec/features/view_result_spec.rb
+++ spec/features/view_result_spec.rb
@@ -2,5 +2,6 @@ feature 'Viewing result' do
   scenario 'Can see repo names' do
     enter_name
     expect(page).to have_content('hello-world')
+    expect(page).to have_content('JavaScript')
   end
 end
