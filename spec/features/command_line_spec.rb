6740f95382e99b2b3e2bcfbebc0bfa3967756a93
TBrockster <tobybrock93@gmail.com>
2019-09-27 12:27:31 +0100

    refactor of previous commit

--- spec/features/command_line_spec.rb
+++ spec/features/command_line_spec.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'graphql_querier'
 
 describe 'integration test' do
- it 'works for user TBrockster on the command line' do
+  it 'works for user TBrockster on the command line' do
     my_graphql_querier = GraphQLQuerier.new
     my_graphql_querier.send_graphql_query('TBrockster')
     expect(my_graphql_querier.map_response).to be_an Array
- end
+  end
 end
--- spec/units/graphql_querier_spec.rb
+++ spec/units/graphql_querier_spec.rb
@@ -6,7 +6,7 @@ describe GraphQLQuerier do
   subject(:graphql_querier) { described_class.new }
   let(:fake_graphql_library) { double :fake_graphql_library }
   let(:fake_response) { double :fake_response }
- 
+
   describe '#send_graphql_query' do
     it 'input double receives the .query method' do
       expect(fake_graphql_library).to receive(:query)
