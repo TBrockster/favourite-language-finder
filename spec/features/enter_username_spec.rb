9ad494e0355d4e117200bed05872395449d942cd
TBrockster <tobybrock93@gmail.com>
2019-09-28 19:06:20 +0100

    fail page has a back button

--- spec/features/enter_username_spec.rb
+++ spec/features/enter_username_spec.rb
@@ -10,5 +10,7 @@ feature 'Entering User:' do
     enter_name(' ')
     expect(page.status_code).to eq(200)
     expect(page).to have_content("I'm sorry, we couldn't find that user.")
+    click_button 'Back'
+    expect(current_path).to eq '/'
   end
 end
